f0c08fd35a16245e9b09887e68a8ab1cbc134ad9
Rosemarie Day <whyarntrobotsincontrolofearth@gmail.com>
2015-05-29 09:28:42 -0400

    Shoe Converter project starting with mens shoe size

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // variables
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+ 
+    // convert button pressed
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        
+        let sizeFromTextField = mensShoeSizeTextField.text.toInt()!
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false                   // make unhidden label
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+        mensShoeSizeTextField.text = "" // clears text field to placeholder text 
+        
+    }
 
 }
 
